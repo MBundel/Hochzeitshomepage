dfb661a27743119cebe5fc0f2bfcf6422beb4e62
MichelBundel <michel@bundel.eu>
2023-08-01 14:36:58 +0200

    something is happening

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideStorage,getStorage } from '@angular/fire/storage';
+import { AngularFireModule} from '@angular/fire/compat';
+import { NavbarComponent } from './site_components/navbar/navbar.component';
+import { FooterComponent } from './site_components/footer/footer.component';
+import { HomeComponent } from './sites/home/home.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // provideFirebaseApp(() => initializeApp(environment.firebase)),
+    // provideAnalytics(() => getAnalytics()),
+    // provideStorage(() => getStorage())
   ],
-  providers: [],
+  providers: [
+    // ScreenTrackingService,UserTrackingService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
