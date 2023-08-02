9602f6b2344fa98b4c911c37ac4938a2ae1a9208
MichelBundel <michel@bundel.eu>
2023-08-02 14:56:49 +0200

    roses are red

--- src/app/sites/portfolio/portfolio.component.ts
+++ src/app/sites/portfolio/portfolio.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.css', '../../../styles/general.scss']
+  styleUrls: ['./portfolio.scss', '../../../styles/general.scss']
 })
 export class PortfolioComponent {
 
