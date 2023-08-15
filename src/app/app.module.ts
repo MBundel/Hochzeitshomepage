77fb1a8288dc80822c3ddd8cda17593943a455c9
MichelBundel <michel@bundel.eu>
2023-08-15 15:19:19 +0200

    call to action & stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
-import { environment } from '../environments/environment';
-import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
-import { provideStorage,getStorage } from '@angular/fire/storage';
-import { AngularFireModule} from '@angular/fire/compat';
+// import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+// import { environment } from '../environments/environment';
+// import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+// import { provideStorage,getStorage } from '@angular/fire/storage';
+// import { AngularFireModule} from '@angular/fire/compat';
 import { NavbarComponent } from './site_components/navbar/navbar.component';
 import { FooterComponent } from './site_components/footer/footer.component';
 import { HomeComponent } from './sites/home/home.component';
