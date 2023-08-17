553fd420ff324b90abcda48d5337bb0ee541f323
MichelBundel <michel@bundel.eu>
2023-08-17 13:57:39 +0200

    icons

--- src/app/site_components/footer/footer.component.ts
+++ src/app/site_components/footer/footer.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
 
+  links = [
+    { routerLink: '/impressum', iconUrl: '/assets/icons/impressum.svg', iconWidth: 50, name: 'Impressum' },
+    { href: 'https://www.instagram.com/capturemotions_/', iconUrl: '/assets/icons/insta.svg', iconWidth: 50, name: 'Instagram' },
+    { href: 'https://bundel.eu', iconUrl: '/assets/icons/home.svg', iconWidth: 70, name: 'Hauptseite' },
+    { routerLink: '/kontakt', iconUrl: '/assets/icons/contact.svg', iconWidth: 80, name: 'Kontakt' },
+    { routerLink: '/impressum', iconUrl: '/assets/icons/privacy.svg', iconWidth: 80, name: 'Datenschutz' },
+    { href: 'https://github.com/MBundel/Hochzeitshomepage', iconUrl: '/assets/icons/git.svg', iconWidth: 50, name: 'Git Repository' }
+  ];
+
 }
