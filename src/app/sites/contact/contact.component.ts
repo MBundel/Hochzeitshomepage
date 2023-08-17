f5327f794d323b02ec0cc6cc822be0db410c86c7
MichelBundel <michel@bundel.eu>
2023-08-17 17:28:21 +0200

    icons

--- src/app/sites/contact/contact.component.ts
+++ src/app/sites/contact/contact.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['../../../styles/icon.scss', '../../../styles/general.scss']
 })
 export class ContactComponent {
-
+  contactLinks = [
+    { href: 'https://wa.me/4917684415706', iconUrl: '/assets/icons/whatsAppLight.svg', iconWidth: 90 },
+    { href: 'tel:+4917684415706', iconUrl: '/assets/icons/callWhite.svg', iconWidth: 100 },
+    { href: 'mailto:michel@bundel.eu', iconUrl: '/assets/icons/emailLight.svg', iconWidth: 100 }
+  ];
 }
