c01450bcfee67ae36a9d97822da7a937a422c55c
MichelBundel <michel@bundel.eu>
2023-08-22 12:06:59 +0200

    navbar

--- src/app/site_components/navbar/navbar.component.ts
+++ src/app/site_components/navbar/navbar.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['../../../styles/general.scss', './navbar.component.scss']
 })
 export class NavbarComponent {
+  isMenuOpen: boolean = false;
 
 }
